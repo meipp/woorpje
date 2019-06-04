eb901a0b000e535955d36e1434c4b196d4a32ccb
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2019-06-04 11:25:10 +0200

    BUild murmur when not available in repo

--- cmake_modules/FindMurmur.cmake
+++ cmake_modules/FindMurmur.cmake
@@ -9,6 +9,20 @@ IF(LIBMURMUR_FOUND)
 	SET(MURMUR_LIBRARIES ${LIBMURMUR_LIBRARY} )
 	SET(MURMUR_INCLUDE_DIRS ${LIBMURMUR_INCLUDE_DIR})
 ELSE(LIBMURMUR_FOUND)
-	SET(MURMUR_LIBRARIES)
-	SET(MURMUR_INCLUDE_DIRS)
+	include(ExternalProject)
+	ExternalProject_Add(murmurhash
+  	URL "https://github.com/kloetzl/libmurmurhash/releases/download/v1.5/libmurmurhash-1.5.tar.gz"
+	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/murmurhash
+	BUILD_IN_SOURCE 1
+	CONFIGURE_COMMAND ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/murmurhash/install/
+	BUILD_COMMAND make
+	INSTALL_COMMAND make install
+	)
+
+	add_library(murmur STATIC IMPORTED)	
+	add_dependencies(murmur murmurhash)
+	set_property(TARGET murmur PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/murmurhash/install/lib/libmurmurhash.so)
+	
+	SET(MURMUR_LIBRARIES  murmur)
+	SET(MURMUR_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/murmurhash/install/include)
 ENDIF(LIBMURMUR_FOUND)
